98180a6100943fff0b87b3ae7184fdd78df804f0
Lyagushka <rayz.sir@gmail.com>
2023-04-27 21:33:14 +0400

    script is working

--- syncswap.js
+++ syncswap.js
@@ -1,7 +1,7 @@
 const Web3 = require('web3');
 const fs = require('fs');
 
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+const web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.era.zksync.io"));
 
 const routerAddress = "0x2da10A1e27bF85cEdD8FFb1AbBe97e53391C0295";
 const abiFile = fs.readFileSync("RouterAbi.json");
@@ -23,30 +23,43 @@ const tokenA = '0x5aea5775959fbc2557cc8789bc1bf90a239d9a91'; // WETH
 const tokenB = '0x3355df6d4c9c3035724fd0e3914de96a5a83aaf4'; // USDC
 const value = web3.utils.toWei('0.0001', 'ether');
 
-const getPoolAddress = async () => {
+const start = async () => {
+    // get pool address
+    // params:
+    // tokenA: address of token A
+    // tokenB: address of token B
     const poolAddress = await poolFactory.methods.getPool(tokenA, tokenB).call();
-    return poolAddress;
-}
+    console.log(poolAddress);
 
-const swapData = defaultAbiCoder.encode(
-    ['address', 'address', 'uint8'],
-    [tokenA, account.address, 1],
-);
 
-const steps = [{
-    pool: getPoolAddress(),
-    data: swapData,
-    callback: ZERO_ADDRESS,
-    callbackData: '0x',
-}];
+    // params:
+    // tokenIn: address of token to swap from
+    // To: address to send swapped tokens to
+    // WithdrawMode: 0 - vault internal transfer, 1 - withdraw and unwrap to naitve ETH, 2 - withdraw and wrap to wETH
+    const swapData = web3.eth.abi.encodeParameters(
+        ['address', 'address', 'uint8'],
+        [tokenA, account.address, 1],
+    );
 
-const paths = [{
-    steps: steps,
-    tokenIn: ZERO_ADDRESS,
-    amountIn: value,
-}];
+    const steps = [{
+        pool: poolAddress, // address of needed pool
+        data: swapData, // encoded swap data
+        callback: ZERO_ADDRESS, // dunno when it's needed
+        callbackData: '0x',
+    }];
 
-const swap = async () => {
+    // If we want to use the native ETH as the input token,
+    // the `tokenIn` on path should be replaced with the zero address.
+    const paths = [{
+        steps: steps,
+        tokenIn: ZERO_ADDRESS, 
+        amountIn: value, // amount of tokens to swap
+    }];
+
+    // params:
+    // paths: array of paths
+    // minAmountOut: minimum amount of tokens to receive aka slippage
+    // deadline: deadline of trade in unix timestamp
     const tx = await router.methods.swap(
         paths,
         0, // min amount out / slippage
@@ -55,9 +68,9 @@ const swap = async () => {
         from: account.address,
         value: value,
         gas: 2000000,
-        gasPrice: web3.utils.toWei('3', 'gwei'),
+        gasPrice: web3.utils.toWei('4', 'gwei'),
     });
-    console.log(tx);
+    console.log(tx.transactionHash);
 }
 
-swap();
+start();
